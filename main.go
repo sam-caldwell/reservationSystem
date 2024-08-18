d48b2c579d89b8b61dd97a2a0bd71382ee399643
Sam Caldwell <github@samcaldwell.net>
2024-08-17 19:44:07 -0500

    remove wasm experiment

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/sam-caldwell/reservationSystem/api"
-	"github.com/sam-caldwell/reservationSystem/ui"
 	"log"
 	"net/http"
 )
@@ -12,13 +11,6 @@ func main() {
 	log.Println("service starting...")
 
 	http.HandleFunc("/api/v1/reservations", api.Router)
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		api.HandleCORS(w, r)
-		log.Println("index page load")
-		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte(ui.IndexHtml))
-	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
